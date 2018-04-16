93a9e293e78f740f5d2eea6883b837c8f2c61365
Anjan Amarnath <aamarnat@usc.edu>
2018-04-16 00:55:31 -0700

    Update AddClass.java

--- src/CS201GroupProject/AddClass.java
+++ src/CS201GroupProject/AddClass.java
@@ -1,6 +1,7 @@
 package CS201GroupProject;
 
 import java.io.IOException;
+import java.util.Vector;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,14 +19,33 @@ public class AddClass extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String pageToForward = null;
 		User currUser = (User) (request.getSession().getAttribute("currUser"));
 		int userID = currUser.getUserID();
 		String courseNum = request.getParameter("courseID");
 		int courseID = Integer.parseInt(courseNum);
 		
-		JDBCQuery.addCourseToUser(courseID, userID);
+		Vector<Integer> usersCourses = JDBCQuery.getCoursesByUserID(currUser.getUserID());
+		boolean validAdd = true;
 		
-		RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/UserClassList.jsp");
+		for(int i = 0; i < usersCourses.size(); i++)  {
+			if(usersCourses.get(i) == courseID) {
+				validAdd = false;
+				break;
+			}
+		}
+		
+		if(validAdd) {
+			JDBCQuery.addCourseToUser(courseID, userID);
+			pageToForward = "/UserClassList.jsp";
+		}
+		
+		else {
+			request.setAttribute("add_err" + courseID, "This class is already in your course list.");
+			pageToForward = "/ClassSearch.jsp";
+		}
+		
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(pageToForward);
 		dispatch.forward(request,  response);
 
 	}
