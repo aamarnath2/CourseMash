d0e66155baa51992d6b1c4dea4e97d3e37bab6be
Cullen Coyle <cullenco@usc.edu>
2018-04-18 18:14:55 -0700

    Update LoginValidation.java

--- src/CS201GroupProject/LoginValidation.java
+++ src/CS201GroupProject/LoginValidation.java
@@ -15,58 +15,67 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 /**
  * Servlet implementation class LoginValidation
  */
 @WebServlet("/LoginValidation")
 public class LoginValidation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
- 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String email = request.getParameter("emailAddress"); //get parameters from form
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//get email address and password
+		String email = request.getParameter("emailAddress"); // get parameters from form
 		String pword = request.getParameter("pword");
-		
+		//default values
 		boolean validLogin = true;
 		String pageToForward = null;
 		
-		if(email == null || email.length() == 0) {
+		//if the user did not enter an email to login, return error
+		if (email == null || email.length() == 0) {
 			request.setAttribute("emailAddresss", email);
 			request.setAttribute("email_err", "Please enter an email");
 			pageToForward = "/home.jsp";
 			validLogin = false;
 		}
-		
-		if(pword == null || pword.length() == 0) {
+		//if the user did not enter a password to login, return error
+		if (pword == null || pword.length() == 0) {
 			request.setAttribute("pass_err", "Please enter a password");
 			pageToForward = "/home.jsp";
 			validLogin = false;
 		}
-		
-		if(validLogin) {
-			if(JDBCQuery.doesUserExist(email)) {
-				if(JDBCQuery.validate(email, pword)) {
-					request.getSession().setAttribute("currUser", JDBCQuery.getUserByEmail(email)); //change function name accordingly
+		//will only execute if the user filled the text fields
+		if (validLogin) {
+			//check if the email exists. Else, return error
+			if (JDBCQuery.doesUserExist(email)) {
+				
+				//now, validate if the email password combo is valid in the database. Else return error
+				if (JDBCQuery.validate(email, pword)) {
+					request.getSession().setAttribute("currUser", JDBCQuery.getUserByEmail(email));
 					request.getSession().setAttribute("signedIn", true);
 					pageToForward = "/UserClassList.jsp";
-				}
+				} 
 				else {
 					request.setAttribute("pass_err", "Incorrect password");
 					pageToForward = "/home.jsp";
-				}
-			}
+				}	
+				
+			} 
+			//user email doesn't exist
 			else {
 				request.setAttribute("email_err", "No user with this email exits");
 				pageToForward = "/home.jsp";
 			}
 		}
-		else { 
+		//else, just go back to home.jsp
+		else {
 			pageToForward = "/home.jsp";
 		}
+		
+		//send servlet dispatch
 		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(pageToForward);
-		dispatch.forward(request,  response);
+		dispatch.forward(request, response);
 	}
 
 }
