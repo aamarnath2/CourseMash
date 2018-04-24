679effc66b26eacf21f56be7a199f6d91e0d5cdf
Cullen Coyle <cullenco@usc.edu>
2018-04-23 17:19:35 -0700

    Update validPost.java

--- src/CS201GroupProject/validPost.java
+++ src/CS201GroupProject/validPost.java
@@ -18,9 +18,11 @@ public class validPost extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//title and body
 		String title = request.getParameter("title");
 		String body = request.getParameter("body");
-		System.out.println("here: " + body);
+		//course info
 		String courseNum = request.getParameter("courseID");
 		Integer courseID = Integer.parseInt(courseNum);
 		User currUser = ((User)request.getSession().getAttribute("currUser"));
@@ -28,40 +30,34 @@ public class validPost extends HttpServlet {
 		
 		boolean validPost = true;
 		
-		
+		//if there is no title
 		if(title.equals("") || title.equals(null)) {
 			request.setAttribute("title_err", "Please enter a title");
-			//request.setAttribute("classid", courseNum);
-			//pageToForward = "/ClassPage.jsp";
 			validPost = false;
 		}
-		
+		//if there is not body
 		if(body.equals("") || body.equals(null)) {
-			System.out.println("helllllllllllo");
 			request.setAttribute("body_err", "Please enter a body");
-			//request.setAttribute("classid", courseNum);
-			//pageToForward = "/ClassPage.jsp";
 			validPost = false;
 		}
-		  
+		 //if there is a title and body for the post 
 		if(validPost) {
 			JDBCQuery.addPost(courseID, currUser.getUserID(), title, body);
 			response.setContentType("text");
 			PrintWriter out = response.getWriter();
 			response.getWriter().write("valid");
-			//RequestDispatcher dispatch = getServletContext().getRequestDispatcher(pageToForward);
-			//dispatch.forward(request,response);
 		}
+		//redisplay info provided
 		else {
 			response.setContentType("text/html");
 		    PrintWriter out = response.getWriter();
 		    response.getWriter().write("<form id='newPostForm' method='GET' action='validPost'>");
-		    response.getWriter().write("<input type=\"text\" name=\"title\" id=\"title\" placeholder=\"title\" value =\"" + title + "\">");
-		    response.getWriter().write("<input type=\"text\" name=\"body\" id=\"body\" placeholder = \"body\" value=\"" + body + "\">");
+		    response.getWriter().write("<input type=\"text\" name=\"title\" id=\"title\" placeholder=\"title\" value =\"" + title + "\"><br>");
+		    response.getWriter().write("<textarea name=\"body\" id=\"body\" placeholder = \"body\" value=\"" + body + "\"></textarea><br>");
 		    response.getWriter().write("<input type=\"button\" id=\"button\" onclick=\"postValidate()\" value=\"Submit\" >");
 		    response.getWriter().write("<input type=\"hidden\" name=\"courseID\" value=\"" + courseNum + "\">");
 		    response.getWriter().write("</form>");
-		    response.getWriter().write("<h1 id=\"testmessage\"> ERROR </h1>");
+		    response.getWriter().write("<h1 id=\"testmessage\"> ERROR: Please enter a title AND body </h1>");
 		}
 	}
 }
