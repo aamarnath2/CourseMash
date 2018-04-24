b386b5f9751cbbb0ba93db17524a774c83dcc091
Cullen Coyle <cullenco@usc.edu>
2018-04-23 17:21:01 -0700

    Update GetPost.java

--- src/CS201GroupProject/GetPost.java
+++ src/CS201GroupProject/GetPost.java
@@ -2,6 +2,7 @@ package CS201GroupProject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,13 +23,26 @@ public class GetPost extends HttpServlet {
 		String postNum = request.getParameter("postid");
 		int postID = Integer.parseInt(postNum);	
 		Post thisPost = JDBCQuery.getPostByPostID(postID);
+		Vector<Reply> replies = JDBCQuery.getRepliesByPostID(postID); //replies
 		
 		
 		response.setContentType("text/html");
 	    PrintWriter out = response.getWriter();
-	    response.getWriter().write("<h2>" + thisPost.getTitle() + "</h2>");
-	    response.getWriter().write("<h3>" + (JDBCQuery.getUserByUserID(thisPost.getUserID())).getName() + "</h3>");
-	    response.getWriter().write("<p>" + thisPost.getBody() + "</p>");
+	    //get post information
+	    response.getWriter().write("<h2 id=\"postTitle\">" + thisPost.getTitle() + "</h2>");
+	    response.getWriter().write("<h3 id=\"postCreator\">" + (JDBCQuery.getUserByUserID(thisPost.getUserID())).getName() + "</h3>");
+	    response.getWriter().write("<p id=\"postBody\">" + thisPost.getBodY() + "</p>");
+	    if(replies != null) {
+	    	//post replies
+		    for(int i = 0; i < replies.size(); i++) {
+		    	String currReply = replies.get(i).getReply();
+		    	response.getWriter().write("<h4 id=\"reply\">" + currReply + "</h4>");
+		    }
+	    }
+	    response.getWriter().write("<form id=\"newReply\" method=\"GET\" action=\"validReply\">"
+	    							+ "<input type=\"text\" name=\"reply\" placeholder=\"reply\"><br>"
+	    							+ "<input type=\"submit\" id=\"replyButton\" value=\"Post Reply\">"
+	    							+ "</form>");
 	   
 	}
 
